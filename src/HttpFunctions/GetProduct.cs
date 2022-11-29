035d49d9acae7b23099bd8514206d6f7a38eea6d
Kosta Petan <kosta.petan@microsoft.com>
2022-11-29 13:36:06 +0000

    add the second challenge endpoint

--- src/HttpFunctions/GetProduct.cs
+++ src/HttpFunctions/GetProduct.cs
@@ -2,6 +2,7 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Web;
 
 namespace HttpFunctions
 {
@@ -15,15 +16,15 @@ namespace HttpFunctions
         }
 
         [Function("GetProduct")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-            response.WriteString("Welcome to Azure Functions!");
-
+            var queryParams = HttpUtility.ParseQueryString(req.Url.Query);
+            var productId = queryParams["productId"];
+            response.WriteString($"The product name for your product id {productId} is Starfruit Explosion");
             return response;
         }
     }
