bf9a9dc15f467ccf9afbf975bb5ff2f9c96f36fd
Kosta Petan <kosta.petan@microsoft.com>
2022-12-01 14:59:46 +0000

    WIP

--- src/HttpFunctions/Program.cs
+++ src/HttpFunctions/Program.cs
@@ -1,14 +1,22 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Azure.Messaging.ServiceBus;
+using Azure.Storage.Blobs;
 
 var sbConnection = Environment.GetEnvironmentVariable("ServiceBusConnection");
+var storageConnectionString = Environment.GetEnvironmentVariable("StorageAccountConnection");
+var storageContainerName = Openhack.MS.Consts.BlobStoragePath;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(s =>
     {
        s.AddSingleton<ServiceBusClient>(new ServiceBusClient(sbConnection));
+       s.AddSingleton<BlobContainerClient>( (x) => {
+            var container = new BlobContainerClient(storageConnectionString, storageContainerName);
+            container.Create();
+            return container;
+       } );
     })
     .Build();
 
--- src/HttpFunctions/RegularReceipts.cs
+++ src/HttpFunctions/RegularReceipts.cs
@@ -1,33 +1,42 @@
 using System;
+using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace Openhack.MS
 {
     public class RegularReceipts
     {
         private readonly ILogger _logger;
+        private readonly BlobContainerClient _container;
 
-        public RegularReceipts(ILoggerFactory loggerFactory)
+        public RegularReceipts(ILoggerFactory loggerFactory, BlobContainerClient container)
         {
             _logger = loggerFactory.CreateLogger<RegularReceipts>();
+            _container = container;
         }
 
         [Function("RegularReceipts")]
-        [BlobOutput(Consts.BlobStoragePath + "/{rand-guid}-output.txt", Consts.StorageAccountConnectionStringSetting)]
-        public RegularBlob Run([ServiceBusTrigger(Consts.ServiceBusTopic, Consts.RegularSubscription, Connection = Consts.SerbiceBusConnectionStringSetting)] SalesMessage mySbMsg)
+        public async Task Run([ServiceBusTrigger(Consts.ServiceBusTopic, Consts.RegularSubscription, Connection = Consts.SerbiceBusConnectionStringSetting)] SalesMessage mySbMsg)
         {
             _logger.LogInformation($"C# ServiceBus topic trigger function processed message with total items: {mySbMsg.totalItems}");
 
-            var blob = new RegularBlob();
+            var result = new RegularBlob
+            {
+                Items = mySbMsg.totalItems,
+                TotalCost = mySbMsg.totalCost,
+                SalesNumber = mySbMsg.salesNumber,
+                SalesDate = mySbMsg.salesDate,
+                Store = mySbMsg.storeLocation
+            };
 
-            blob.Items = mySBMsg.totalItems;
-            blob.TotalCost = mySBMsg.totalCost;
-            blob.SalesNumber = mySBMsg.salesNumber;
-            blob.SalesDate = mySBMsg.salesDate;
-            blob.Store = mySBMsg.storeLocation;
-
-            return blob;
+            var serializedPayload = JsonSerializer.Serialize(result);
+            var stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(serializedPayload));
+            
+            string blobName = "sample-blob";
+            var blob = _container.GetBlobClient(blobName);
+            await blob.UploadAsync(stream);
         }
 
     }
@@ -35,9 +44,9 @@ namespace Openhack.MS
     public class RegularBlob
     {
         public int Items { get; set; }
-        public string TotalCost { get; set; }
+        public decimal TotalCost { get; set; }
         public string SalesNumber { get; set; }
-        public string SalesDate { get; set; }
+        public DateTime SalesDate { get; set; }
         public string Store { get; set; }
     }
 }
