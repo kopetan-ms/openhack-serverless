8e157fde1989802cd422b7f44485d71826b13378
Kosta Petan <kosta.petan@microsoft.com>
2022-12-01 13:44:07 +0000

    PoS sales sends messages

--- src/HttpFunctions/Program.cs
+++ src/HttpFunctions/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Azure.Messaging.ServiceBus;
+
+var sbConnection = Environment.GetEnvironmentVariable("ServiceBusConnection");
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(s =>
     {
-       
+       s.AddSingleton<ServiceBusClient>(new ServiceBusClient(sbConnection));
     })
     .Build();
 
